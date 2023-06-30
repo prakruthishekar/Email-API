f887d1ac721f379c56fea0046b8ea3e7ae157565
somashekarprakruthi <114468337+somashekarprakruthi@users.noreply.github.com>
2023-06-29 21:59:28 -0400

    first commit

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = MAIL_USERNAME
-app.config['MAIL_PASSWORD'] = MAIL_PASSWORD
+app.config['MAIL_USERNAME'] = "bostonhealthservice@gmail.com"
+app.config['MAIL_PASSWORD'] = "flemhtqfurzhrvmo"
 
 mail = Mail(app)
 
